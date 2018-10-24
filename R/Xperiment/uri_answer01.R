3f8b23fbc9b77db4be406b7150aa2c7cfc16e50e
natgoodman <natg@shore.net>
2018-10-24 12:49:54 -0700

    snapshot commit for Dropbox workaround

--- R/Xperiment/uri_answer01.R
+++ R/Xperiment/uri_answer01.R
@@ -19,30 +19,37 @@
 source('R/repwr.R');
 ## --- Eperimental sandbox functions ---
 ## run for this sandbox
-run=function(save.fig=T,clean.fig=save.fig,...) {
-  init_xperiment(save.fig=T,clean.fig=save.fig,...);
-  ## dodata(need.init=F,...);              # use repwr data
-  dodoc(need.init=F,docfun=doc_xperiment,...);  # generate figures for doc
+run=function(need.init=T,...) {
+  if (need.init) wrap_fun(init_xperiment);
+  need.init=F;
+  ## wrap_fun(dodata);                # use repwr data
+  wrap_fun(dodoc,init_doc_xperiment,doc='xperiment'); # generate figures, tables for doc
 }
-## init for this sandbox
-init_xperiment=init_uri_answer01=
-  function(doc='xperiment',...) {
-    subdoc='uri_answer01';
-    init(doc='repwr',
-         ## use repwr data and other params
-         figdir=filename('figure','xperiment',subdoc,'m=1e4'),...);
-  }
+## init for this sandbox. same parameters as rewpr
+init_xperiment=function(doc='repwr',...) {
+  ## call init with our arguments
+  wrap_fun(init);
+}
+## init_doc for this sandbox
+init_doc_xperiment=
+  function(doc='xperiment',subdoc='uri_answer01',docfun=doc_xperiment,
+           figdir=filename('figure',doc,subdoc,mdir),
+           tbldir=filename('table',doc,subdoc,mdir),
+           clean.out=T,figscreen=T,...) {
+   doc<<-'xperiment';                    # bodily set doc to keep init_doc happy
+   wrap_fun(init_doc);
+ }
+
 ## make figures and tables for experimental sandbox code
 ## sect is which sections to run - for use during development
 ##   uses prefix matching and all matches run
-doc_xperiment=doc_uri_answer01=
-  function(sect=parent(sect,NULL),
-           fignum=parent(fignum,1),figscreen=parent(figscreen,T),fignew=parent(fignew,F)) {
-    sect.all=c('20-50','20-500');
-    if (is.null(sect)) sect=sect.all else sect=pmatch_choice(sect,sect.all);
-    mesr=cq(sig2,d2.scp1);
+doc_xperiment=function(sect=parent(sect,NULL)) {
+  sect.all=c('20-50','20-500');
+  if (is.null(sect)) sect=sect.all else sect=pmatch_choice(sect,sect.all);
+  mesr=cq(sig2,d2.scp1);
+  sapply(sect,function(sect) {
 ##### 20-50: n1=20, n2=50
-    if ((figsect='20-50') %in% sect) {
+    if (sect=='20-50') {
       n1=20; n2=50; d1=d_sig(n1,0.05); d2=seq(0,1,0.1);
       d.33=uniroot(function(d0) p_d2t(n1,d=d1,d0,lower.tail=F)-0.33,interval=c(0,10))$root;
       d.scp=uniroot(function(d) p_d2t(n2,d,d0=d.33)-0.05,c(-10,10))$root;
@@ -50,9 +57,9 @@ doc_xperiment=doc_uri_answer01=
             d1=0,d2=d2,n1=n1,n2=n2,vline=c(d.scp,d.33),vhlty='dashed',legend='topright');
       dofig(plotrate,'20-50_fnr',rate.rule='nonzro',rate.type='error',mesr=mesr,
             d1=0.5,d2=d2,n1=n1,n2=n2,vline=c(d.scp,d.33),vhlty='dashed',legend='topright');
-   }
+    }
 ##### 20-500: n1=20, n2=500
-    if ((figsect='20-500') %in% sect) {
+    if (sect=='20-500') {
       n1=20; n2=500; d1=d_sig(n1,0.05); d2=seq(0,1,0.1);
       d.33=uniroot(function(d0) p_d2t(n1,d=d1,d0,lower.tail=F)-0.33,interval=c(0,10))$root;
       d.scp=uniroot(function(d) p_d2t(n2,d,d0=d.33)-0.05,c(-10,10))$root;
@@ -62,4 +69,5 @@ doc_xperiment=doc_uri_answer01=
             d1=0.5,d2=d2,n1=n1,n2=n2,vline=c(d.scp,d.33),vhlty='dashed',legend='topright');
     }
     sect;
-  }
+  })
+}
